a15f387d1e9bc7444fd40fb5b4c6beec97909506
Infant Ajay Venus <infantcsajay@outlook.com>
2024-01-29 21:22:41 +0530

    [auth_screen] feat: modify width responsively

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -32,77 +32,104 @@ class _AuthScreenState extends State<AuthScreen> {
           ),
         ),
         child: Center(
-          child: Card(
-            margin: const EdgeInsets.all(10),
-            clipBehavior: Clip.hardEdge,
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 50,
-                    width: double.infinity,
-                    child: Row(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              print('DEBUG:CONSTRAINTS: ${constraints.maxWidth}');
+              var widthFactor = 1.0;
+              if (constraints.maxWidth > 1780) {
+                widthFactor = 0.3;
+              } else if (constraints.maxWidth > 1440) {
+                widthFactor = 0.4;
+              } else if (constraints.maxWidth > 1080) {
+                widthFactor = 0.5;
+              } else if (constraints.maxWidth > 720) {
+                widthFactor = 0.7;
+              } else if (constraints.maxWidth > 540) {
+                widthFactor = 0.8;
+              } else if (constraints.maxWidth > 480) {
+                widthFactor = 0.9;
+              }
+
+              return FractionallySizedBox(
+                widthFactor: widthFactor,
+                child: Card(
+                  margin: const EdgeInsets.all(10),
+                  clipBehavior: Clip.hardEdge,
+                  child: SingleChildScrollView(
+                    child: Column(
                       children: [
-                        Expanded(
-                          child: Container(
-                            decoration: !_isLogin
-                                ? BoxDecoration(
-                                    border: Border(
-                                      bottom: BorderSide(
-                                        color: Theme.of(context).primaryColor,
-                                      ),
-                                    ),
-                                    color: Theme.of(context)
-                                        .buttonTheme
-                                        .colorScheme!
-                                        .secondaryContainer,
-                                  )
-                                : null,
-                            child: TextButton(
-                              onPressed: () {
-                                setState(() {
-                                  _isLogin = true;
-                                });
-                              },
-                              child: const Text("Login"),
-                            ),
+                        SizedBox(
+                          height: 50,
+                          width: double.infinity,
+                          child: Row(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Expanded(
+                                child: Container(
+                                  height: double.infinity,
+                                  width: double.infinity,
+                                  decoration: !_isLogin
+                                      ? BoxDecoration(
+                                          border: Border(
+                                            bottom: BorderSide(
+                                              color: Theme.of(context)
+                                                  .primaryColor,
+                                            ),
+                                          ),
+                                          color: Theme.of(context)
+                                              .buttonTheme
+                                              .colorScheme!
+                                              .secondaryContainer,
+                                        )
+                                      : null,
+                                  child: TextButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        _isLogin = true;
+                                      });
+                                    },
+                                    child: const Text("Login"),
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                child: Container(
+                                  height: double.infinity,
+                                  decoration: _isLogin
+                                      ? BoxDecoration(
+                                          border: Border(
+                                            bottom: BorderSide(
+                                              color: Theme.of(context)
+                                                  .primaryColor,
+                                            ),
+                                          ),
+                                          color: Theme.of(context)
+                                              .buttonTheme
+                                              .colorScheme!
+                                              .secondaryContainer,
+                                        )
+                                      : null,
+                                  child: TextButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        _isLogin = false;
+                                      });
+                                    },
+                                    child: const Text("SignUp"),
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                        Expanded(
-                          child: Container(
-                            height: double.infinity,
-                            decoration: _isLogin
-                                ? BoxDecoration(
-                                    border: Border(
-                                      bottom: BorderSide(
-                                        color: Theme.of(context).primaryColor,
-                                      ),
-                                    ),
-                                    color: Theme.of(context)
-                                        .buttonTheme
-                                        .colorScheme!
-                                        .secondaryContainer,
-                                  )
-                                : null,
-                            child: TextButton(
-                              onPressed: () {
-                                setState(() {
-                                  _isLogin = false;
-                                });
-                              },
-                              child: const Text("SignUp"),
-                            ),
-                          ),
-                        ),
+                        _isLogin ? const LoginForm() : const SignUpForm()
                       ],
                     ),
                   ),
-                  _isLogin ? const LoginForm() : const SignUpForm()
-                ],
-              ),
-            ),
+                ),
+              );
+            },
           ),
         ),
       ),
